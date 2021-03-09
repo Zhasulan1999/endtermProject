c19ecbb296421ba9c119aaf412544a17f342b24b
Zhasulan1999 <jace.ubaidulla@gmail.com>
2021-03-09 20:14:01 +0600

    updated version

--- src/com/company/repositories/interfaces/IAppointmentRepository.java
+++ src/com/company/repositories/interfaces/IAppointmentRepository.java
@@ -1,5 +1,12 @@
 package com.company.repositories.interfaces;
 
-public interface IAppointmentRepository {
+import com.company.entities.Appointment;
 
+import java.util.List;
+
+public interface IAppointmentRepository {
+    boolean createAppointment(Appointment appointment);
+    int getRoomByAppointmentId(int app_id);
+    String getAppointmentByDate(String date);
+    List<Appointment> getAllAppointments();
 }
